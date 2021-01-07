16f045ceddea34b7657205072fe24e0e9db2171e
Jack Gordley <jgordley99@gmail.com>
2021-01-06 19:23:55 -0500

    Adding profile info table

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -1,10 +1,12 @@
 import S3Stack from "./S3Stack";
 import CognitoStack from "./CognitoStack";
 import DynamoDBStack from "./DynamoDBStack";
+import DynamoDBProfileStack from "./DynamoDBProfileStack";
 
 // Add stacks
 export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
+  new DynamoDBProfileStack(app, "dynamodb");
 
   const s3 = new S3Stack(app, "s3");
 
