9a1d5fd18beea6fd381c43ad275f8eaa8c01313e
Jack Gordley <jgordley99@gmail.com>
2021-01-08 18:15:50 -0500

    Adding dynamoDB stack to hold company information

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -2,11 +2,13 @@ import S3Stack from "./S3Stack";
 import CognitoStack from "./CognitoStack";
 import DynamoDBStack from "./DynamoDBStack";
 import DynamoDBProfileStack from "./DynamoDBProfileStack";
+import DynamoDBCompanyStack from "./DynamoDBCompanyStack";
 
 // Add stacks
 export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
   new DynamoDBProfileStack(app, "profiles");
+  new DynamoDBCompanyStack(app, "company");
 
   const s3 = new S3Stack(app, "s3");
 
